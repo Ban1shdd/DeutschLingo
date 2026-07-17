24ca60d9b70f0f8d41a488911e0631d371f348d1
Ban1shdd <saloons_percent_1a@icloud.com>
2026-07-17 19:27:59 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'deutschlingo-v1';
+const assets = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+});
